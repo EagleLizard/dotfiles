9da8cfcc8eaa80b0b3e7ed9de17445e3eedccda0
eaglelizard <whitedtd@gmail.com>
2024-06-19 16:18:45 -0600

    run chsh as sudo

--- pi/pi-setup.sh
+++ pi/pi-setup.sh
@@ -22,7 +22,7 @@ sudo apt install zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # set zsh as default
-chsh -s $(which zsh)
+sudo chsh -s $(which zsh) $USER
 
 # install zsh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
